dcb68d541d4b6cfd013d98d17990c0735b44538f
CJ29DJ <32182005+CJ29DJ@users.noreply.github.com>
2017-09-22 08:56:58 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
 
     tests_require=['nose', 'requests_mock'],
 
-    url='https://github.com/dtcarls/ff_bot',
+    url='https://github.com/CJ29DJ/ff_bot',
 
     classifiers=[
         'Natural Language :: English',
