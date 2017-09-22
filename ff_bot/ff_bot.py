7cfb5cd30967611d4fbaf680af1b3a3f47d2d07e
CJ29DJ <32182005+CJ29DJ@users.noreply.github.com>
2017-09-21 19:27:27 -0500

    Update ff_bot.py

--- ff_bot/ff_bot.py
+++ ff_bot/ff_bot.py
@@ -60,43 +60,21 @@ def get_scoreboard(league):
     text = ['Score Update'] + score
     return '\n'.join(text)
 
-def get_matchups(league):
-    '''Gets current week's Matchups'''
-    matchups = league.scoreboard()
-    
-    '''TODO: NORMALIZE STRING LENGTH'''
-    score = ['%s(%s-%s) vs %s(%s-%s)' % (i.home_team.team_name, i.home_team.wins, i.home_team.losses,
-             i.away_team.team_name, i.away_team.wins, i.away_team.losses) for i in matchups
-             if i.away_team]
-    text = ['This Week\'s Matchups'] + score + '\n' + random_phrase()
-    return '\n'.join(text)
-
 def get_close_scores(league):
-    '''Gets current closest scores (15.999 points or closer)'''
+    '''Gets current closest scores (10.000 points or closer)'''
     matchups = league.scoreboard()
     score = []
     
     for i in matchups:
         if i.away_team:
             diffScore = i.away_score - i.home_score
-            if -16 < diffScore < 16:
+            if -10 < diffScore < 10:
                 '''TODO: NORMALIZE STRING LENGTH'''
                 score += ['%s %.2f - %.2f %s' % (i.home_team.team_abbrev, i.home_score,
                         i.away_score, i.away_team.team_abbrev)]
     if not score:
         score = ['None']
     text = ['Close Scores'] + score
-    return '\n'.join(text)
-
-def get_power_rankings(league):
-    '''Gets current week's power rankings'''
-    '''Using 2 step dominance, as well as a combination of points scored and margin of victory. 
-    It's weighted 80/15/5 respectively'''
-    pranks = league.power_rankings(week=4)
-    
-    score = ['%s - %s' % (i[0], i[1].team_name) for i in pranks
-             if i]
-    text = ['This Week\'s Power Rankings'] + score
     return '\n'.join(text)
 
 def bot_main(function):
@@ -121,9 +99,6 @@ def bot_main(function):
         bot.send_message(text)
     elif function=="get_close_scores":
         text = get_close_scores(league)
-        bot.send_message(text)
-    elif function=="get_power_rankings":
-        text = get_power_rankings(league)
         bot.send_message(text)
     elif function=="init":
         try:
@@ -151,26 +126,22 @@ if __name__ == '__main__':
     try:
         myTimezone = os.environ["TIMEZONE"]
     except:
-        myTimezone='America/New_York'
+        myTimezone='America/Chicago'
 
     bot_main("init")
     sched = BlockingScheduler(job_defaults={'misfire_grace_time': 15*60})
     '''
-    power rankings go out tuesday evening at 6:30pm. 
     matchups go out thursday evening at 7:30pm.
-    close scores (within 15.99 points) go out monday evening at 6:30pm. 
-    score update friday, monday, and tuesday morning at 12:30am.
-    score update sunday at 1pm, 4pm, 8pm. 
+    close scores (within 10.00 points) go out monday at 7:00am. 
+    score update sunday at 6:30pm. 
     
     '''
 
     '''sched.add_job(bot_main, 'interval', ['get_matchups'], seconds=30, id='get_matchups_test', replace_existing=True)'''
     '''sched.add_job(bot_main, 'cron', ['get_scoreboard_short'], id='test', day_of_week='thu', hour='14', minute=50, timezone=myTimezone, replace_existing=True)'''
     
-    sched.add_job(bot_main, 'cron', ['get_power_rankings'], id='power_rankings', day_of_week='tue', hour=18, minute=30, start_date=ff_start_date, end_date=ff_end_date, timezone=myTimezone, replace_existing=True)
     sched.add_job(bot_main, 'cron', ['get_matchups'], id='matchups', day_of_week='thu', hour=19, minute=30, start_date=ff_start_date, end_date=ff_end_date, timezone=myTimezone, replace_existing=True)
-    sched.add_job(bot_main, 'cron', ['get_close_scores'], id='close_scores', day_of_week='mon', hour=18, minute=30, start_date=ff_start_date, end_date=ff_end_date, timezone=myTimezone, replace_existing=True)
-    sched.add_job(bot_main, 'cron', ['get_scoreboard_short'], id='scoreboard1', day_of_week='fri,mon,tue', hour=7, minute=30, start_date=ff_start_date, end_date=ff_end_date, timezone=myTimezone, replace_existing=True)
-    sched.add_job(bot_main, 'cron', ['get_scoreboard_short'], id='scoreboard2', day_of_week='sun', hour='16,20', start_date=ff_start_date, end_date=ff_end_date, timezone=myTimezone, replace_existing=True)
+    sched.add_job(bot_main, 'cron', ['get_close_scores'], id='close_scores', day_of_week='mon', hour=7, start_date=ff_start_date, end_date=ff_end_date, timezone=myTimezone, replace_existing=True)
+    sched.add_job(bot_main, 'cron', ['get_scoreboard_short'], id='scoreboard2', day_of_week='sun', hour='18', minute=30, start_date=ff_start_date, end_date=ff_end_date, timezone=myTimezone, replace_existing=True)
 
     sched.start()
